36123519e06288601b7818d825c026cf28086fd0
pmbfsa <paulomarcioba@gmail.com>
2026-06-09 11:36:08 -0300

    Add all page elements styles Add Font Awesome SASS files and webfonts Add vite plugin webfont-dl Add google fonts Comfortaa, Kaushan Scropt and Oswald

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    webfontDownload(
+      'https://fonts.googleapis.com/css2?family=Comfortaa&family=Kaushan+Script&family=Oswald&display=swap',
+      { assetsSubfolder: 'fonts' },
+    ),
+  ],
 });
