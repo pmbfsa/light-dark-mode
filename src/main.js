659457ea6d5d0039f8c7d7fe38847d5ac4dcd260
pmbfsa <paulomarcioba@gmail.com>
2026-06-09 23:06:52 -0300

    Optimize main.js code structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './sass/style.scss';
 
+// Constants
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+
+// DOM Elements
 const toggleThemeSwitch = document.querySelector('input[type="checkbox"]');
 const title = document.getElementById('title');
 const subtitle = document.getElementById('subtitle');
@@ -10,57 +15,64 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-// Get images for Light or Dark Mode
-function changeImageMode(mode) {
+// Get images for Light or Dark Theme
+function changeImageTheme(mode) {
   image1.src = `./svgs/undraw_web_developer_${mode}.svg`;
   image2.src = `./svgs/undraw_click_interaction_${mode}.svg`;
   image3.src = `./svgs/undraw_conference_call_${mode}.svg`;
 }
 
-// Change elements styles to Dark Mode
-function darkMode() {
-  title.textContent = 'Welcome to the dark side!';
-  subtitle.textContent = 'Enjoy yourself in the realm of shadow';
-  nav.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-  textBox.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  changeImageMode('dark');
+// Toggle elements properties to match Light and Dark Theme
+function toggleLightDarkTheme(theme) {
+  title.textContent =
+    theme === LIGHT_THEME
+      ? 'Welcome to the light side!'
+      : 'Welcome to the dark side!';
+
+  subtitle.textContent =
+    theme === LIGHT_THEME
+      ? 'Enjoy yourself in the realm of brightness'
+      : 'Enjoy yourself in the realm of shadow';
+
+  nav.style.backgroundColor =
+    theme === LIGHT_THEME ? 'rgba(255, 255, 255, 0.5)' : 'rgba(0, 0, 0, 0.5)';
+
+  textBox.style.backgroundColor =
+    theme === LIGHT_THEME ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)';
+
+  toggleIcon.children[0].textContent =
+    theme === LIGHT_THEME ? 'Light Mode' : 'Dark Mode';
+
+  theme === LIGHT_THEME
+    ? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+
+  changeImageTheme(theme);
 }
 
-// Change elements styles to Light Mode
-function lightMode() {
-  title.textContent = 'Welcome to the light side!';
-  subtitle.textContent = 'Enjoy yourself in the realm of brightness';
-  nav.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-  textBox.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  changeImageMode('light');
-}
+// Theme Switch Change Event
+function onChangeThemeSwitch(event) {
+  document.documentElement.setAttribute(
+    'data-theme',
+    event.target.checked ? DARK_THEME : LIGHT_THEME,
+  );
 
-function switchTheme(event) {
-  if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-    darkMode();
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-    lightMode();
-  }
+  localStorage.setItem(
+    'theme',
+    event.target.checked ? DARK_THEME : LIGHT_THEME,
+  );
+
+  toggleLightDarkTheme(event.target.checked ? DARK_THEME : LIGHT_THEME);
 }
 
 // Event listeners
-toggleThemeSwitch.addEventListener('change', switchTheme);
+toggleThemeSwitch.addEventListener('change', onChangeThemeSwitch);
 
+// On load
 const theme = localStorage.getItem('theme');
 
 if (theme) {
   document.documentElement.setAttribute('data-theme', theme);
-
-  if (theme === 'dark') {
-    toggleThemeSwitch.checked = true;
-    darkMode();
-  }
+  toggleThemeSwitch.checked = theme === DARK_THEME;
+  toggleLightDarkTheme(theme);
 }
